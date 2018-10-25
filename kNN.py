78df411ec29a0ddcb8a0cec922f367b801ce6441
Kubpica <kubpicapf@gmail.com>
2018-10-26 00:35:06 +0200

    Hello world!

---
+++ kNN.py
@@ -0,0 +1,7 @@
+class KNN(object):
+    u"klasa implementująca algorytm kNN realizujący zadanie klasyfikacji z metryką Euklidesowską"
+    def __init__(self):
+        print("hello")
+
+obiekt = KNN()
+print("world")
