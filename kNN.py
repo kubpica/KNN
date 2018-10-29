f77baa7466a704e5c1f74068646ac469849586e4
Kubpica <kubpicapf@gmail.com>
2018-10-29 20:15:16 +0100

    Predict method for kNN

--- kNN.py
+++ kNN.py
@@ -3,16 +3,46 @@ import numpy as np
 import math as math
 import operator
 from scipy.spatial.distance import euclidean
+from collections import Counter
 
 class KNN(object):
     u"Klasa implementująca algorytm kNN realizujący zadanie klasyfikacji z metryką Euklidesowską."
-    def __init__(self, list, k):
+    def __init__(self, learnData, k):
         print("hello")
         self.k = k
-        self.list = list
+        self.list = learnData
 
-    def predict(self, list):
+    def predict(self, testData):
         print("predict")
+        xLearn = np.array(self.list.ix[:, 0:4])
+        yLearn = np.array(self.list['class'])
+        xTest = np.array(testData.ix[:, 0:4])
+        #yTest = np.array(testData.list['class'])
+
+        listToReturn = []
+
+        for i in range(len(xTest)):
+            distances = []
+            # Compute distances
+            for j in range(len(xLearn)):
+                distance = euclidean(xTest[i], xLearn[j])
+                distances.append([distance, j])
+
+            # Sort distances
+            distances = sorted(distances)
+
+            # Target K nearest neighbors
+            targets = []
+            for j in range(self.k):
+                index = distances[j][1]
+                targets.append(yLearn[index])
+
+            # Append most common target
+            listToReturn.append(Counter(targets).most_common(1)[0][0])
+
+        return listToReturn
+
+
 
     def score(self, list):
         print("score")
--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import pandas as pd
 import kNN
 
 print("hi")
-testData = pd.read_csv('data/iris.data.test', sep=',', header=None)
-learnData = pd.read_csv('data/iris.data.learning', sep=',', header=None)
+
+names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class']
+testData = pd.read_csv('data/iris.data.test', sep=',', header=None, names=names)
+learnData = pd.read_csv('data/iris.data.learning', sep=',', header=None, names=names)
+
 knn = kNN.KNN(learnData, 5)
-knn.predict(testData)
+print(knn.predict(testData))
