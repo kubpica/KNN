8f25aff7a322015285d93ffdf2e74efad8fa62f8
Kubpica <kubpicapf@gmail.com>
2018-10-29 17:37:16 +0100

    Imports

--- kNN.py
+++ kNN.py
@@ -1,5 +1,10 @@
+import pandas as pd
+import numpy as np
+import math as math
+import operator
+
 class KNN(object):
-    u"klasa implementująca algorytm kNN realizujący zadanie klasyfikacji z metryką Euklidesowską"
+    u"Klasa implementująca algorytm kNN realizujący zadanie klasyfikacji z metryką Euklidesowską."
     def __init__(self):
         print("hello")
 
